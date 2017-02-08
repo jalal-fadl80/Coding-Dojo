fe74a1478a1beeccc43cc063c9e92e958c1cfc50
jalal-fadl <jalal-forever@yahoo.com>
2017-02-07 18:30:21 -0800

    Adding Some type of looping

---
+++ C# assignments/dictionaries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           Dictionary<string,string> profile = new Dictionary<string,string>();
+           profile.Add("Name","Speros");
+           profile.Add("Language","PHP");
+           profile.Add("Location","Greece");
+           System.Console.WriteLine("        Instructor Profile");
+           System.Console.WriteLine("        Name-" + profile["Name"]);
+           System.Console.WriteLine("        From-" + profile["Location"]);
+           System.Console.WriteLine("        Favourite language-" + profile["Language"]);
+            foreach(KeyValuePair<string,string> entry in profile) {
+             System.Console.WriteLine(entry.Key + " - " + entry.Value);
+        
+           }
+        }
+    }
+}
